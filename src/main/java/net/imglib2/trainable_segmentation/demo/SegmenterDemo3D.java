ed09066123ab559ec71b7ca84924ac24a0b7c8a9
Matthias Arzt <arzt@mpi-cbg.de>
2017-10-25 20:13:13 +0200

    Beautify SegmenterDemo3D

--- src/main/java/net/imglib2/trainable_segmentation/demo/SegmenterDemo3D.java
+++ src/main/java/net/imglib2/trainable_segmentation/demo/SegmenterDemo3D.java
@@ -1,5 +1,6 @@
 package net.imglib2.trainable_segmentation.demo;
 
+import bdv.util.Bdv;
 import bdv.util.BdvFunctions;
 import bdv.util.BdvOptions;
 import bdv.util.BdvStackSource;
@@ -15,6 +16,7 @@ import net.imglib2.trainable_segmention.pixel_feature.filter.GroupedFeatures;
 import net.imglib2.trainable_segmention.pixel_feature.filter.SingleFeatures;
 import net.imglib2.trainable_segmention.pixel_feature.settings.FeatureSettings;
 import net.imglib2.trainable_segmention.pixel_feature.settings.GlobalSettings;
+import net.imglib2.type.numeric.ARGBType;
 import net.imglib2.type.numeric.IntegerType;
 import net.imglib2.type.numeric.integer.ByteType;
 import net.imglib2.type.numeric.integer.IntType;
@@ -24,6 +26,7 @@ import net.imglib2.util.Intervals;
 import net.imglib2.view.Views;
 import org.scijava.Context;
 
+import java.awt.*;
 import java.util.Arrays;
 
 public class SegmenterDemo3D
@@ -56,6 +59,8 @@ public class SegmenterDemo3D
 				globals,
 				SingleFeatures.identity(),
 				GroupedFeatures.gauss()//,
+		// NB: Using the other features is to slow.
+		// It's possible to speed it by using tiling and lazy evaluation
 				//GroupedFeatures.hessian3D(false),
 				//GroupedFeatures.lipschitz(50 ),
 				//GroupedFeatures.differenceOfGaussians(),
@@ -70,9 +75,17 @@ public class SegmenterDemo3D
 		Segmenter segmenter = Trainer.train( imagej.op(), image, labeling, settings );
 		Img< ByteType > result = segmenter.segment( image );
 
-		BdvStackSource< FloatType > bdv = BdvFunctions.show( image, "image" );
-		BdvFunctions.show( labelIndices,"labeling", BdvOptions.options().addTo( bdv ) );
-		BdvFunctions.show( result,"segmentation", BdvOptions.options().addTo( bdv ) );
+		Bdv bdv = showImage( "image", image, Color.red, 200, null );
+		showImage( "labeling", labelIndices, Color.blue, 2, bdv );
+		showImage( "segmentation", result, Color.green, 2, bdv );
+	}
+
+	private < T > Bdv showImage( String text, RandomAccessibleInterval< T > result, Color color, double maxIntensity, Bdv bdv )
+	{
+		BdvStackSource< T > b = BdvFunctions.show( result, text, BdvOptions.options().addTo( bdv ) );
+		b.setColor( new ARGBType( color.getRGB() ) );
+		b.setDisplayRange( 0, maxIntensity );
+		return b;
 	}
 
 	// -- Helper methods --
